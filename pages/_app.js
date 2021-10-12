2977c786325d9f26ccbfd23b6f240d9eeb50a7f0
made-it-out <mike.hallisy@gmail.com>
2021-10-11 20:47:40 -0400

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.0/css/bootstrap-grid.min.css" integrity="sha512-3AxW5HcDzhL9MJdO2mpDEGEZ6NcCg/pDSa8R2kH5gwEA4r48RxZf0nPITA1NfX1pNA6a/eAayX+yW6QopF4jeg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+        <title>Accordions</title>
       </Head>
       <Component {...pageProps} />
     </>
